fcac0a56ff7b1722895aed858a9e1a4394110a11
CatsLoveTw <105958410+CatsLoveTw@users.noreply.github.com>
2022-10-09 19:46:02 +0800

    移除不必要的內容 / readme更新

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # RPG-World
- ![version](https://img.shields.io/badge/Minecraft_Version-Bedrock_1.18.30+-brightgreen.svg)
+![version](https://img.shields.io/badge/Minecraft_Version-Bedrock_1.19.30+-brightgreen.svg)
 
- 正在製作的RPG地圖
- 支持版本:V1.19.31
- 可能支持的版本:V1.19.3x
-
+> 本地圖所附帶的ScriptApi 手機電腦皆可使用
+### 說明 / 聲明
+本專案/檔案為存放用，歡迎使用/修改專案\
+但**須遵守以下規則**:
+- 修改/轉載時請標註專案來源及作者
+- 此專案不得稱呼為自己的作品
+### 未修復的問題
+- 無
+### 預計更新
+- 完成大廳
+- 新增隊伍系統
+- 完成第一關場景
+- 完善經驗增加途徑
+- 完善等級系統 (獎勵等功能)
+- 劇情新增
+- 新增角色
+- 更多的技能組...
--- behavior_packs/Typescript Pack/scripts/commands/update.js
+++ behavior_packs/Typescript Pack/scripts/commands/update.js
@@ -6,51 +6,6 @@ import { tpa } from './tpa.js'
 
 // | 或 §
 
-// var updates = [
-//     'Beta 0.1 : 開始製作',
-//     'Beta 0.2 : 新增UI系統',
-//     'Beta 0.5 : 新增加入通知',
-//     'Beta 0.6 : 新增自訂義指令',
-//     'Beta 0.8 : 新增聊天訊息修改',
-//     'Beta 1.0 : 更改訊息內容',
-//     'Beta 1.3 : 新增廣播',
-//     'Beta 1.5 : 將管理員廣播內容顯示於actionbar',
-//     'Beta 2.0 : 新增掛機偵測',
-//     'Beta 2.5 : 新增排行榜',
-//     'Beta 2.6 : 修復部分bug (顯示問題等)',
-//     'Beta 3.0 : 修復Bug 更改內容 新增可開關式功能',
-//     'Beta 3.2 : 新增轉帳功能',
-//     'Beta 3.5 : 新增等級功能',
-//     'Beta 3.75 : 修復部分bug',
-//     'Beta 4.0 : 新增銀行系統',
-//     'Beta 4.15 : 修復銀行系統偵測物品Bug',
-//     'Beta 4.2 : 移除測試時的顯示訊息',
-//     'Beta 4.5 : 新增設定自己的稱號系統',
-//     'Beta 5.0 : 修改顯示等級&商店功能',
-//     'Beta 5.3 : 新增鐵匠商店 (iron_ui) & 礦物商店 (mine_ui)',
-//     'Beta 5.3 repair : 修復某些稱號顯示問題',
-//     'Beta 5.5 : 簡易外掛系統',
-//     'Beta 6.0 : 新增抽獎系統',
-//     'Beta 6.0 repair : 更改抽獎機率&修復玩家選單無法抽獎的問題',
-//     'Beta 6.5 : 新增夜錠商店系統',
-//     'Beta 7.0 : 新增商品(夜錠商店)',
-//     'Beta 7.0 Repair : 抽獎機刷錢問題修復&挖掘加速粒子效果隱藏&修改外掛處決方式',
-//     'Beta 7.1 : 新增現在時間功能',
-//     'Beta 7.2 : 移除管理員在排行榜的排名',
-//     'Beta 8.0 : 將相容性轉為生存伺服器&新增領地系統(未完成)',
-//     'Beta 8.05 : 領地系統玩家',
-//     'Beta 8.5 : 新增領地系統'
-// ]
-// export function update () {
-//     var colorupdate = []
-//     var getversion = updates[updates.length-1].split(':')[0]
-//     for (let i in updates) {
-//         colorupdate.push(`§g§l${updates[i].split(":")[0]}\n§b§l${updates[i].split(':')[1]}`)
-//     }
-    
-//     return {"version": getversion, "update": colorupdate}
-// }
-
 export const updateMenu = (player) => {
     let Update = {
         "V0.1": ["大廳完成80%%", "新增血量 | 傷害系統"],
